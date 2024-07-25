6b01ed08ea7667edb6e5bcc837f4ed81bc25e482
Eglantine Clervil <cj291957@ens.uqam.ca>
2024-07-25 11:07:36 -0400

    Ajouter squelette du fichier 'main.c'

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+
+#define USAGE "NAME\n" \
+               "    %s - Trie une liste de mots provenant d’un fichier d’entrée.\n" \
+               "    Une fois les mots triés, le programme affichera la liste ordonnée\n" \
+               "    sans doublons sur stdout.\n" \
+               "\n" \
+               "SYNOPSIS\n" \
+               "    %s entree.txt [-S sortie.txt]\n" \
+               "\n" \
+               "DESCRIPTION\n" \
+               "    entree.txt\n" \
+               "        Fichier contenant la liste des mots à trier.\n" \
+               "    -S sortie.txt\n" \
+               "        (Optionnel) Génère des statistiques et les enregistre\n" \
+               "        dans le fichier de sortie spécifié.\n"
+
+
+
+int main (int argc, char **argv) {
+    
+
+
+return 0;
+}
