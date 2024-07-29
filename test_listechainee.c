57f85d0f516d902e68bf2be49d49efd51dba5329
Eglantine Clervil <cj291957@ens.uqam.ca>
2024-07-28 22:19:22 -0400

    Ajouter 'test_free_word_list' dans 'test_listechainee.c'

--- test_listechainee.c
+++ test_listechainee.c
@@ -57,7 +57,61 @@ void test_insert_in_order(void) {
     CU_ASSERT_STRING_EQUAL(word4, mock_list.head->next->next->word);
     CU_ASSERT_STRING_EQUAL(word1, mock_list.head->next->word);
     CU_ASSERT_STRING_EQUAL(word5, mock_list.head->next->next->next->word);
+    free_word_list(mock_list.head);
+}
 
+void test_free_list(void) {
+    LinkedList mock_list;
+    initialize_list(&mock_list);
+
+    CU_ASSERT(mock_list.word_count == 0);
+    const char *word1 = "CHAT";
+    const char *word2 = "MAISON";
+    const char *word3 = "AVION";
+
+    insert_in_order(&mock_list, word1);
+    insert_in_order(&mock_list, word2);
+    insert_in_order(&mock_list, word3);
+
+    CU_ASSERT_EQUAL(3, mock_list.word_count);
+    free_word_list(mock_list.head);
+    CU_ASSERT_EQUAL(0, mock_list.word_count);
+    CU_ASSERT_PTR_EQUAL(mock_list.head, NULL);
+    CU_ASSERT_PTR_EQUAL(mock_list.tail, NULL);
+
+
+}
+
+void test_delete_duplicate(void) {
+    LinkedList mock_list;
+    initialize_list(&mock_list);
+
+    const char *word1 = "CHAT";
+    const char *word2 = "MAISON";
+    const char *word3 = "AVION";
+    const char *word4 = "GLACE";
+    const char *word5 = "LUNE";
+    const char *word6 = "LUNE";
+    const char *word7 = "AVION";
+
+    insert_in_order(&mock_list, word1);
+    insert_in_order(&mock_list, word2);
+    insert_in_order(&mock_list, word3);
+    insert_in_order(&mock_list, word4);
+    insert_in_order(&mock_list, word5);
+    insert_in_order(&mock_list, word6);
+    insert_in_order(&mock_list, word7);
+
+    // Comparer AVION à AVION
+    CU_ASSERT_STRING_EQUAL(mock_list.head->word, mock_list.head->next->word);
+    CU_ASSERT(mock_list.word_count == 7);
+
+    delete_duplicate(&mock_list);
+    
+    CU_ASSERT(mock_list.word_count == 5);
+    CU_ASSERT_STRING_EQUAL(word1, mock_list.head->next->word);
+
+    free_word_list(mock_list.head);
 
 
 }
@@ -66,10 +120,12 @@ int main() {
     CU_initialize_registry();
 
     CU_pSuite suite = CU_add_suite("LinkedList Suite", 0, 0);
-    CU_add_test(suite, "test of initialize_list()", test_initialize_list);
-    CU_add_test(suite, "test of insert_into_empty_list()", test_insert_into_empty_list);
-    CU_add_test(suite, "test of create_node()", test_create_node);
-    CU_add_test(suite, "test of insert_in_order()", test_insert_in_order);
+    CU_add_test(suite, "test of initialize_list", test_initialize_list);
+    CU_add_test(suite, "test of insert_into_empty_list", test_insert_into_empty_list);
+    CU_add_test(suite, "test of create_node", test_create_node);
+    CU_add_test(suite, "test of insert_in_order", test_insert_in_order);
+    CU_add_test(suite, "test of delete_duplicate", test_delete_duplicate);
+    CU_add_test(suite, "test of free_word_list", test_free_list);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
